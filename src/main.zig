bf357beebb1927124062e35ca40687a66144062a
mJehanno <mathob.jehanno@hotmail.fr>
2024-12-25 18:52:08 +0100

    feat(folder-struct): add folder-struct generation feature
    other function like create-file are not stable yet

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,34 @@
 const std = @import("std");
 const fs = std.fs;
+const os = std.os;
 const extism_pdk = @import("extism-pdk");
 const Plugin = extism_pdk.Plugin;
 
 const allocator = std.heap.wasm_allocator;
+const cwd = fs.Dir{ .fd = fs.defaultWasiCwd() };
 
-export fn create_file() i32 {
+const typeEnum = enum {
+    file,
+    folder,
+};
+
+const Spec = union(enum) {
+    content: []u8,
+    children: []Filer,
+};
+
+const Filer = struct {
+    name: []u8,
+    type: typeEnum,
+    spec: ?Spec = null,
+};
+
+export fn @"create-file"() i32 {
     const plugin = Plugin.init(allocator);
     const filename = plugin.getInput() catch unreachable;
     defer allocator.free(filename);
 
-    const file = fs.cwd().createFile(filename, .{ .read = true }) catch |err| {
+    const file = cwd.createFile(filename, .{ .read = true }) catch |err| {
         const out = std.fmt.allocPrint(allocator, "Failed to create file '{s}': {s}", .{ filename, @errorName(err) }) catch unreachable;
         plugin.setError(out);
         return 1;
@@ -20,7 +38,7 @@ export fn create_file() i32 {
     return 0;
 }
 
-export fn write_file() i32 {
+export fn @"write-file"() i32 {
     const plugin = Plugin.init(allocator);
     const Input = struct {
         filename: []u8,
@@ -29,7 +47,7 @@ export fn write_file() i32 {
 
     const input = plugin.getJson(Input) catch unreachable;
 
-    fs.cwd().writeFile(fs.Dir.WriteFileOptions{ .sub_path = input.filename, .data = input.content }) catch |err| {
+    cwd.writeFile(fs.Dir.WriteFileOptions{ .sub_path = input.filename, .data = input.content }) catch |err| {
         const out = std.fmt.allocPrint(allocator, "Failed to write file '{s}': {s}", .{ input.filename, @errorName(err) }) catch unreachable;
         plugin.setError(out);
         return 1;
@@ -37,11 +55,12 @@ export fn write_file() i32 {
     return 0;
 }
 
-export fn create_folder() i32 {
+export fn @"create-folder"() i32 {
     const plugin = Plugin.init(allocator);
     const foldername = plugin.getInput() catch unreachable;
     defer allocator.free(foldername);
-    fs.cwd().makeDir(foldername) catch |err| {
+
+    cwd.makeDir(foldername) catch |err| {
         const out = std.fmt.allocPrint(allocator, "Failed to create folder '{s}': {s}", .{ foldername, @errorName(err) }) catch unreachable;
         plugin.setError(out);
         return 1;
@@ -50,19 +69,66 @@ export fn create_folder() i32 {
     return 0;
 }
 
-export fn create_folder_struct() i32 {
+fn handleErr(p: Plugin, message: []const u8, err: anyerror) i32 {
+    const out = std.fmt.allocPrint(allocator, "{s}: {s}", .{ message, @errorName(err) }) catch unreachable;
+    p.setError(out);
+    return 1;
+}
+
+fn parseConfigFolderStruct(str: []u8) anyerror![]Filer {
+    const params = try std.json.parseFromSlice([]Filer, allocator, str, std.json.ParseOptions{});
+    return params.value;
+}
+
+export fn @"create-folder-struct"() callconv(.C) i32 {
     const plugin = Plugin.init(allocator);
-    const conf = plugin.getConfig("folder_struct") catch unreachable orelse {
-        plugin.setError("this action requires a folder_struct to be defined");
-        return 1;
+
+    const config = plugin.getConfig("folder_struct") catch |err| {
+        return handleErr(plugin, "this action requires a folder_struct to be defined", err);
     };
-    plugin.log(.Info, conf);
+    if (config) |conf| {
+        plugin.log(.Info, std.fmt.allocPrint(allocator, "{s}", .{conf}) catch unreachable);
+        const filers = parseConfigFolderStruct(conf) catch |err| {
+            return handleErr(plugin, "failed to parse folder_struct", err);
+        };
 
-    // _ = std.json.parseFromSlice(i32, allocator, conf, std.json.ParseOptions{}) catch |err| {
-    //     const out = std.fmt.allocPrint(allocator, "Failed to parse config: {s}", .{@errorName(err)}) catch unreachable;
-    //     plugin.setError(out);
-    //     return 1;
-    // };
+        return create_folder_struct(plugin, filers, cwd);
+    }
 
+    return 0;
+}
+
+fn create_folder_struct(plugin: Plugin, filers: []Filer, current: fs.Dir) i32 {
+    for (filers) |filer| {
+        switch (filer.type) {
+            .file => {
+                const fd = current.createFile(filer.name, .{ .read = true, .truncate = true }) catch |err| {
+                    const out = std.fmt.allocPrint(allocator, "failed to create file {s}", .{filer.name}) catch unreachable;
+                    return handleErr(plugin, out, err);
+                };
+                defer fd.close();
+
+                if (filer.spec) |spec| {
+                    if (!std.mem.eql(u8, spec.content, "")) {
+                        fd.writeAll(spec.content) catch |err| {
+                            const out = std.fmt.allocPrint(allocator, "failed to write content to file {s}", .{filer.name}) catch unreachable;
+                            return handleErr(plugin, out, err);
+                        };
+                    }
+                }
+            },
+            .folder => {
+                const next = current.makeOpenPath(filer.name, .{ .access_sub_paths = true }) catch |err| {
+                    const out = std.fmt.allocPrint(allocator, "failed to create folder {s}", .{filer.name}) catch unreachable;
+                    return handleErr(plugin, out, err);
+                };
+                if (filer.spec) |spec| {
+                    if (spec.children.len != 0) {
+                        return create_folder_struct(plugin, spec.children, next);
+                    }
+                }
+            },
+        }
+    }
     return 0;
 }
